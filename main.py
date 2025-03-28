e20c47ed387c3de6197b8ca73f1492a7d7514fe6
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-28 11:55:35 +0000

    main com a logica do envio dos emails

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+from utils.logger_util import get_logger
 from core.email_sender import send_email
+from utils.readcsv import read_contacts
+from utils.caminhos import caminho_csv_colaboradores
 
 
-send_email("tutasoares17@gmail.com", "Tuta")
+logger= get_logger(__name__)
 
-
-
-
-
-
-
-
-
+def main():
     
+    contactos=read_contacts(caminho_csv_colaboradores)
+
+    for contacto in contactos:
+        nome = contacto["nome"]
+        email= contacto["email"]
+        anexo = contacto["anexo"]
+        tipo= contacto["tipo"]
+    
+        try:
+            send_email(destinatario=email, nome=nome, file=anexo, tipo=tipo)
+            logger.info(f"Email enviado com sucesso para {nome} <{email}>")
+
+        except Exception as e:
+            logger.error(f"Erro ao envio email para: {nome} <{email}>: {e}")
+
+if __name__=="__main__":
+    main()
