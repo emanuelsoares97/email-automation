b4e749effa1ce4af0a7cf85cd4e6599574f8fc41
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-28 11:56:44 +0000

    funçao para o envio de emails

--- core/email_sender.py
+++ core/email_sender.py
@@ -1,42 +1,46 @@
-from dotenv import load_dotenv
 import os
 from email.message import EmailMessage
+from core.template_loader import load_template
 import smtplib
-import filetype
+from dotenv import load_dotenv
+from utils.logger_util import get_logger
 
+logger= get_logger(__name__)
 
+def send_email(destinatario, nome=None, file=None, tipo=None):
+    try:
+        load_dotenv()
+        email = os.environ.get("EMAIL")
+        password = os.environ.get("EMAIL_PASSWORD")
 
-def send_email(destinatario, nome=None, file=None):
-    load_dotenv()  # carrega variáveis do .env
-    email = os.environ.get("EMAIL")
-    password = os.environ.get("EMAIL_PASSWORD")
+        msg = EmailMessage()
+        msg["Subject"] = "Mensagem Automática"
+        msg["From"] = email
+        msg["To"] = destinatario
+        msg.set_content("Versão alternativa em texto simples.")
 
-    msg= EmailMessage()
-    msg["Subject"] = "grafico"
-    msg["From"]= email
-    msg["To"]= destinatario
-    msg.set_content("somos uma familia bonita")
+        if tipo:
+            # Carrega o HTML com base no tipo de e-mail (do CSV)
+            html = load_template(f"{tipo}.html", nome)
+            msg.add_alternative(html, subtype="html")
+        else:
+            logger.warning("Tentativa de enviar email sem tipo")
+            raise ValueError("Todos os emails têm de ter um tipo")
 
-    if file:
-        with open(file, "rb") as f:
-            file_data = f.read()
-            file_name = os.path.basename(f.name)
+        # Se tiver anexo, adiciona usando caminho automático da pasta 'pdfs'
+        if file:
+            pdf_path = os.path.join('pdfs', file)
+            with open(pdf_path, "rb") as f:
+                file_data = f.read()
+                file_name = os.path.basename(f.name)
+            msg.add_attachment(file_data, maintype="application", subtype="pdf", filename=file_name)
+            logger.info(f"Anexo adicionado: {file}")
 
-        msg.add_attachment(file_data, maintype="application", subtype="pdf", filename=file_name)
-
-    msg.add_alternative(f"""
-        <!DOCTYPE html>
-        <html>
-
-        <body>
-            <h1 style="color: blue;"> Olá {nome}</h1>
-        </body>
-        </html>   
-        """, subtype="html")
-
-    #fazer o login ao servidor do email
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-
-        smtp.login(email, password)
-
-        smtp.send_message(msg)
+        # 3️⃣ Envia o e-mail
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(email, password)
+            smtp.send_message(msg)
+    
+    except Exception as e:
+        logger.error(f"Tentativa de enviar email, erro: {e}")
+        raise f"Erro ao enviar email: {e}"
