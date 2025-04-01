fed12f96e34aaa2a4c3bb6db866e3a1d39c6f625
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-01 20:25:13 +0100

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -1,10 +1,22 @@
-from app.__init__ import create_app
-
+from app import create_app
+from app.database.database import db
+from app.models.utilizador import Utilizador
+from werkzeug.security import generate_password_hash
 
 app = create_app()
 
-print("✅ App criada:", app)
-print("📦 Tipo do app:", type(app))
-
 with app.app_context():
-    print("🚀 Consegui acessar o app_context!")
+    db.create_all()
+
+    if not Utilizador.query.filter_by(email="admin@email.com").first():
+        admin = Utilizador(
+            nome="Administrador",
+            email="admin@email.com",
+            password=generate_password_hash("admin123"),
+            role="admin"
+        )
+        db.session.add(admin)
+        db.session.commit()
+        print("Admin criado com sucesso.")
+    else:
+        print("O admin já existe.")
