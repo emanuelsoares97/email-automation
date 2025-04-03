c4df6098b6a8ed09eb2424a35437632bd4341060
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-03 17:43:04 +0100

    Update tokenrevogado_manager.py

--- app/services/tokenrevogado_manager.py
+++ app/services/tokenrevogado_manager.py
@@ -1,4 +1,4 @@
-from app.database import Database
+from app.database.database import db
 from app.models.tokenrevogado import TokenRevogado
 from app.utils.logger_util import get_logger
 
@@ -15,12 +15,11 @@ class TokenService:
         token_revogado = TokenRevogado(token_jti=token_jti)
         logger.info(f"Token revogado: {token_revogado}")
 
-        session = Database.get_session()
-        session.add(token_revogado)
-        session.commit()
+        db.session.add(token_revogado)
+        db.session.commit()
 
     @staticmethod
     def esta_na_blacklist(token_jti):
         """Verifica se um token está na blacklist"""
-        session = Database.get_session()
-        return session.query(TokenRevogado).filter_by(token_jti=token_jti).first() is not None
+        
+        return db.session.query(TokenRevogado).filter_by(token_jti=token_jti).first() is not None
