78ae3d9dfebaf060dbf32aca14d8445c92c2dbce
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-04 22:43:12 +0100

    Update tokenrevogado.py

--- app/models/tokenrevogado.py
+++ app/models/tokenrevogado.py
@@ -7,4 +7,7 @@ class TokenRevogado(BaseModel):
 
     id = Column(Integer, primary_key=True)
     token_jti = Column(String, nullable=False, unique=True)  # ID único do token
-    criado_em = Column(DateTime,  default=lambda: datetime.now(timezone.utc))
+    criado_em = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return f"<TokenRevogado(id={self.id}, token_jti={self.token_jti}, criado_em={self.criado_em})>"
