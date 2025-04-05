1154442b8910ebd49b41b7c74a26026760659078
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-05 20:54:21 +0100

    Update sendemail.py

---
+++ app/routes/api/sendemail.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request, jsonify, make_response, g
+from app.services.utilizador_service import UtilizadorService
+from app.services.tokenrevogado_service import TokenService
+from app.utils.logger_util import get_logger
+from app.services.auth_service import AuthService
+from app.models.utilizador import Utilizador
+from app.utils.validacao import validar_email
+import jwt
+from collections import namedtuple
+from config import Config
+
+
+logger = get_logger(__name__)
+
+sendemail_bp = Blueprint("sendemail", __name__)
+
