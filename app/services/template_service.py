df53babfa4f99c58c1bcc70255c36c2863c99791
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-05 20:54:12 +0100

    Update template_service.py

--- app/services/template_service.py
+++ app/services/template_service.py
@@ -1,5 +1,10 @@
 from app.models.template import Template
 from app import db
+from flask import g
+from app.utils.logger_util import get_logger
+from sqlalchemy import or_
+
+logger= get_logger(__name__)
 
 class TemplateService:
 
@@ -22,6 +27,27 @@ class TemplateService:
     @staticmethod
     def get_template(template_id):
         return Template.query.get(template_id)
+
+    @staticmethod
+    def get_list_template(user_id):
+
+        try:
+            logger.info("Lista de templates")
+            
+            if g.current_user["role"]== "admin":
+
+                listas = Template.query.all()
+
+                return [lista.to_dict() for lista in listas]
+            else:
+                
+                listas = Template.query.filter(or_(Template.is_global== True, Template.user_id==user_id)).all()
+
+                return [lista.to_dict() for lista in listas]
+            
+        except Exception as e:
+            logger.error(f"Erro ao tentar buscar a lista de templates com o utilizador: {g.current_user['email']}, erro: {str(e)}", exc_info=True)
+            return ({"erro": "Erro interno no servidor"}), 500
 
     @staticmethod
     def update_template(template_id, data):
