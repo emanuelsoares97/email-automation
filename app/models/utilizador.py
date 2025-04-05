ff78a8d6f23074eb2c7f2024db525eb0d53be27e
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-05 13:47:25 +0100

    Update utilizador.py

--- app/models/utilizador.py
+++ app/models/utilizador.py
@@ -1,19 +1,23 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, UniqueConstraint
 from app.models.abstrata import BaseModel
 from sqlalchemy.orm import relationship
 
 class Utilizador(BaseModel):  
     __tablename__ = "utilizadores"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)  #Garantir que `id` fica primeiro!
+    __table_args__ = (
+    UniqueConstraint("email", name="uq_utilizadores_email"),
+)
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String(100), nullable=False)
-    email= Column(String, nullable=False, unique=True)
-    password=Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
     role = Column(String, default="user") 
     ativo = Column(Boolean, default=True)
 
-    # Relacionamento com o UserPlan para acessar o plano do usuário
-    planos = relationship("UserPlan", backref="utilizador", lazy="dynamic")  # Acessa os planos associados ao utilizador
+    # Relacionamento 
+    planos = relationship("UserPlan", back_populates="utilizador", lazy="dynamic")
 
     def __repr__(self):
         return f'<User {self.nome}>'
