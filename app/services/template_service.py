8259da9e72b9c9b4e2ff0d6c3f6a4cb5c546c398
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-14 19:35:57 +0100

    Update template_service.py

--- app/services/template_service.py
+++ app/services/template_service.py
@@ -73,18 +73,24 @@ class TemplateService:
         user = g.current_user
 
         try:
-            template = Template.query.filter(
-                Template.user_id == user["id"],
-                Template.id == template_id
-            ).first()
+            # Se o user for admin, ele pode editar qualquer template
+            if user["role"] == "admin":
+                template = Template.query.filter(Template.id == template_id).first()
+            else:
+                # Caso contrário, apenas o template do utilizador pode ser atualizado
+                template = Template.query.filter(
+                    Template.user_id == user["id"],
+                    Template.id == template_id
+                ).first()
 
             if template:
+                # Atualiza os campos do template
                 template.name = data.get('name', template.name)
                 template.subject = data.get('subject', template.subject)
                 template.body = data.get('body', template.body)
                 template.is_global = data.get('is_global', template.is_global)
-            
 
+                # Verifica se o novo nome já está em uso (para não duplicar nomes)
                 exist_template = Template.query.filter(
                     Template.name == template.name,
                     Template.user_id == user["id"]
@@ -92,10 +98,14 @@ class TemplateService:
 
                 if exist_template and exist_template.id != template.id:
                     return {"error": "Já existe um template com esse nome, usa outro."}, 400
+                
+                # Se o template for global e o usuário for admin, pode ser atualizado
+                if user["role"] == "admin" and template.is_global == True:
+                    db.session.commit()
 
                 db.session.commit()
                 logger.info(f"Template ID {template_id} atualizado por {user['email']}")
-                return template, 200
+                return template.to_dict(), 200
 
             logger.warning(f"Tentativa inválida de update no template ID {template_id} por {user['email']}")
             return {"erro": "Tentativa de aceder a um template que não pertence ao utilizador"}, 403
@@ -107,11 +117,32 @@ class TemplateService:
 
 
 
+
     @staticmethod
     def delete_template(template_id):
-        template = Template.query.get(template_id)
-        if template:
-            db.session.delete(template)
-            db.session.commit()
-            return True
-        return False
+
+        user = g.current_user
+
+        try:
+        # Se o user for admin, ele pode deletar qualquer template
+            if user["role"] == "admin":
+                template = Template.query.filter(Template.id == template_id).first()
+            else:
+                # Caso contrário, apenas o template do utilizador 
+                template = Template.query.filter(
+                    Template.user_id == user["id"],
+                    Template.id == template_id
+                ).first()
+
+            if template:
+                db.session.delete(template)
+                db.session.commit()
+                return '', 204
+            
+            logger.warning(f"Tentativa inválida de delete no template ID {template_id} por {user['email']}")
+            return {"erro": "Tentativa de aceder a um template que não pertence ao utilizador"}, 403
+
+        except Exception as e:
+            db.session.rollback()
+            logger.error(f"Erro ao atualizar template ID {template_id}: {str(e)}", exc_info=True)
+            return {"erro": "Erro interno ao atualizar template"}, 500
