29f5c51616dff3f475e1c087413810079ef0c45c
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-14 17:56:13 +0100

    Update template_service.py

--- app/services/template_service.py
+++ app/services/template_service.py
@@ -11,20 +11,32 @@ class TemplateService:
     @staticmethod
     def create_template(user, name, subject, body, is_global=False):
 
+        user = g.current_user
+
         try: 
-            if is_global and user.role != 'admin':
-                raise Exception("Somente admins podem criar templates globais.")
+            if is_global and user["role"] != 'admin':
+                raise Exception("Somente admins podem criar templates globais.")            
+
             
             new_template = Template(
-                user_id=None if is_global else user.id,
+                user_id=None if is_global else user["id"],
                 name=name,
                 subject=subject,
                 body=body,
                 is_global=is_global
             )
+
+            exist_template = Template.query.filter(
+                    Template.name == name,
+                    Template.user_id == user["id"]
+                ).first()
+
+            if exist_template:
+                return {"error": "Já existe um template com esse nome, usa outro."}, 400
+
             db.session.add(new_template)
             db.session.commit()
-            return new_template, 201
+            return new_template.to_dict(), 201
         
         except Exception as e:
             logger.error(f"Erro ao tentar criar um template com o utilizador: {g.current_user['email']}, erro: {str(e)}", exc_info=True)
@@ -57,16 +69,43 @@ class TemplateService:
             return ({"erro": "Erro interno no servidor"}), 500
 
     @staticmethod
-    def update_template(template_id, data):
-        template = Template.query.get(template_id)
-        if template:
-            template.name = data.get('name', template.name)
-            template.subject = data.get('subject', template.subject)
-            template.body = data.get('body', template.body)
-            template.is_global = data.get('is_global', template.is_global)
-            db.session.commit()
-            return template
-        return None
+    def update_template(user_id, template_id, data):
+        user = g.current_user
+
+        try:
+            template = Template.query.filter(
+                Template.user_id == user["id"],
+                Template.id == template_id
+            ).first()
+
+            if template:
+                template.name = data.get('name', template.name)
+                template.subject = data.get('subject', template.subject)
+                template.body = data.get('body', template.body)
+                template.is_global = data.get('is_global', template.is_global)
+            
+
+                exist_template = Template.query.filter(
+                    Template.name == template.name,
+                    Template.user_id == user["id"]
+                ).first()
+
+                if exist_template and exist_template.id != template.id:
+                    return {"error": "Já existe um template com esse nome, usa outro."}, 400
+
+                db.session.commit()
+                logger.info(f"Template ID {template_id} atualizado por {user['email']}")
+                return template, 200
+
+            logger.warning(f"Tentativa inválida de update no template ID {template_id} por {user['email']}")
+            return {"erro": "Tentativa de aceder a um template que não pertence ao utilizador"}, 403
+
+        except Exception as e:
+            db.session.rollback()
+            logger.error(f"Erro ao atualizar template ID {template_id}: {str(e)}", exc_info=True)
+            return {"erro": "Erro interno ao atualizar template"}, 500
+
+
 
     @staticmethod
     def delete_template(template_id):
