dd0913d8300a6fd13d0a9af55fb026ffc4c5d7a4
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-15 19:48:50 +0100

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,10 +3,12 @@ import importlib
 from app.database.database import db
 
 def register_models():
-    models_dir = os.path.dirname(__file__)  # Obtém o diretório onde o arquivo atual está (app/models)
+    models_dir = os.path.dirname(__file__)
+    
     for filename in os.listdir(models_dir):
-        # Verifica se é um arquivo Python (e não o __init__.py)
+        
         if filename.endswith(".py") and filename != "__init__.py":
-            module_name = f"app.models.{filename[:-3]}"  # Remove a extensão .py e gera o nome do módulo
-            importlib.import_module(module_name)  # Importa o módulo
+            module_name = f"app.models.{filename[:-3]}"  
+            importlib.import_module(module_name)  
+
     db.create_all()  # Cria as tabelas após o registro
