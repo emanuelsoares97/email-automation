ba208ee266c3d3a5bf3142fe3cb34a9cc7d57b81
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-15 19:48:58 +0100

    Create organization.py

---
+++ app/models/organization.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models.abstrata import BaseModel
+from datetime import datetime, timezone
+
+class Organization(BaseModel):
+
+    __table___tablename__ = "organization"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), unique=True, nullable=False)
+
+    utilizador = relationship("Utilizador", backref="organization", lazy=True)
