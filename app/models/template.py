885e4d6ff908938520fa14ea7248063cce5edae9
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-22 12:45:58 +0100

    Update template.py

--- app/models/template.py
+++ app/models/template.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, Boolean
-from app.models.abstrata import BaseModel
+from app.models.abstract import BaseModel
 from datetime import datetime, timezone
 from sqlalchemy.orm import relationship
 
@@ -7,17 +7,17 @@ class Template(BaseModel):
     __tablename__ = "templates"
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('utilizadores.id'), nullable=True)  # Pode ser NULL para templates gerais
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=True)  # Pode ser NULL para templates gerais
     name = Column(String(255), nullable=False)
     subject = Column(String(255), nullable=False)
     body = Column(Text, nullable=False)
-    criado_em = Column(DateTime, default=lambda: datetime.now())
+    created = Column(DateTime, default=lambda: datetime.now())
     is_global = Column(Boolean, default=False)  # Indica se o template é geral
 
     
-    user_id  = Column(Integer, ForeignKey("utilizadores.id"), nullable=True)
+    user_id  = Column(Integer, ForeignKey("users.id"), nullable=True)
 
-    utilizador = relationship("Utilizador", back_populates="templates")
+    user = relationship("User", back_populates="templates")
 
 
     def __repr__(self):
