b9a44fdf2f1d41d142334fd1174a2dc2df94af89
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-22 12:45:43 +0100

    Update contact.py

--- app/models/contact.py
+++ app/models/contact.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey
-from app.models.abstrata import BaseModel
+from app.models.abstract import BaseModel
 from sqlalchemy.orm import relationship
 from datetime import datetime, timezone
 
@@ -11,11 +11,11 @@ class Contact(BaseModel):
     user_id = Column(Integer, ForeignKey('utilizadores.id'), nullable=False)
     name = Column(String(255), nullable=False)
     email = Column(String(255), nullable=False)
-    criado_em = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    created = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     organization_id = Column(Integer, ForeignKey("organizations.id"), nullable=True)
     area_id = Column(Integer, ForeignKey("areas.id"), nullable=True)
 
 
     organization = relationship("Organization", back_populates="contacts")
-    area= relationship("Area",         back_populates="contacts")
-    utilizador= relationship("Utilizador",  backref="contacts")
+    area= relationship("Area", back_populates="contacts")
+    user = relationship("User", backref="contacts")
