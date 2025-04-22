026377fb5db8335ea1761c7ed8eac238fe70fad7
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-22 12:45:56 +0100

    Update organization.py

--- app/models/organization.py
+++ app/models/organization.py
@@ -1,16 +1,16 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
-from app.models.abstrata import BaseModel
+from app.models.abstract import BaseModel
 from datetime import datetime, timezone
 
 class Organization(BaseModel):
-    
+
     __tablename__ = "organizations"
 
     id   = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
 
-    areas    = relationship("Area",       back_populates="organization", lazy="dynamic")
-    contacts = relationship("Contact",    back_populates="organization", lazy="dynamic")
-    users    = relationship("Utilizador", back_populates="organization", lazy="dynamic")
+    areas = relationship("Area", back_populates="organizations", lazy="dynamic")
+    contacts = relationship("Contact", back_populates="organizations", lazy="dynamic")
+    users = relationship("User", back_populates="organizations", lazy="dynamic")
 
