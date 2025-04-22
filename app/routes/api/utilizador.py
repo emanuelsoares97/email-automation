6b15734824326cae7ca1c235355e52e3d44cac5b
emanuelsoares97 <tutasoares17@gmail.com>
2025-04-22 19:16:07 +0100

    Update utilizador.py

--- app/routes/api/utilizador.py
+++ app/routes/api/utilizador.py
@@ -1,17 +1,17 @@
 from flask import Blueprint, request, jsonify
-from app.services.utilizador_service import UtilizadorService
+from app.services.user_service import UtilizadorService
 from app.services.auth_service import AuthService
 from app.utils.logger_util import get_logger
 from flask import g
 
 logger = get_logger(__name__)
 
-utilizador_bp = Blueprint("utilizador", __name__)
+user_bp = Blueprint("user", __name__)
 
-@utilizador_bp.route("/new", methods=["POST"])
+@user_bp.route("/new", methods=["POST"])
 @AuthService.token_required
 @AuthService.role_required("admin")
-def criar_utilizador():
+def create_new_user():
     """Cria um novo utilizador"""
     try:
         data = request.get_json()
@@ -20,7 +20,7 @@ def criar_utilizador():
             return jsonify({"erro": "Dados inválidos."}), 400
 
         
-        if not data or "nome" not in data or "email" not in data or "password" not in data:
+        if not data or "name" not in data or "email" not in data or "password" not in data:
             logger.warning("Tentativa de criar utilizador sem dados completos.")
             return jsonify({"erro": "Nome, email e senha são obrigatórios!"}), 400
         
@@ -31,8 +31,8 @@ def criar_utilizador():
 
 
         logger.info(f"Tentativa de criar utilizador: {data.get('email')}")
-        resposta, status = UtilizadorService.criar_utilizador(
-                data.get("nome"),
+        resposta, status = UserService.criar_utilizador(
+                data.get("name"),
                 data.get("email"),
                 data.get("password")
         )
